d7431eccb72c19847ad8d20e4e7de2f7bc02c3a5
JeramTough <1171867004@qq.com>
2018-06-04 01:57:31 +0800

    感觉现在的ioc框架很nice

--- app/src/main/java/com/jeramtough/jtandroiddemo/component/B.java
+++ app/src/main/java/com/jeramtough/jtandroiddemo/component/B.java
@@ -2,13 +2,14 @@ package com.jeramtough.jtandroiddemo.component;
 
 import android.content.Context;
 import com.jeramtough.jtandroid.ioc.annotation.IocAutowire;
+import com.jeramtough.jtandroid.ioc.annotation.JtBeanPattern;
 import com.jeramtough.jtandroid.ioc.annotation.JtComponent;
 
 /**
  * @author 11718
  * on 2018  May 04 Friday 02:11.
  */
-@JtComponent
+@JtComponent(pattern = JtBeanPattern.Prototype)
 public class B
 {
 	private Context context;
