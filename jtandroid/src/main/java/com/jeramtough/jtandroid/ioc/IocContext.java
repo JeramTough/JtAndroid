5ff2c82489fafd379c8d7393b3bc45d7ba859f01
JeramTough <1171867004@qq.com>
2019-08-27 15:49:13 +0800

    准备增加新功能

--- jtandroid/src/main/java/com/jeramtough/jtandroid/ioc/IocContext.java
+++ jtandroid/src/main/java/com/jeramtough/jtandroid/ioc/IocContext.java
@@ -5,7 +5,15 @@ package com.jeramtough.jtandroid.ioc;
  */
 public interface IocContext {
 
+    /**
+     * 将容器里的bean注入到对象里
+     *
+     * @param injectedObject 被注入bean的对象
+     */
     void injectBeansInto(Object injectedObject);
 
+    /**
+     * 返回Bean容器
+     */
     BeansContainer getBeansContainer();
 }
